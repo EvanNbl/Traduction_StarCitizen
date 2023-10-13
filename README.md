a3ca0a57ee611a39f0493d91402c1f4bcc132633
Evan <evantvpro@gmail.com>
2023-10-13 23:07:21 +0200

    Add github link

--- README.md
+++ README.md
@@ -6,10 +6,6 @@ Ce projet a pour but de traduire le jeu Star Citizen en français automatiquemen
 
 ## Utilisation
 
-<!-- Une fois le projet téléchargé, double-cliquez sur le fichier `Traduction_StarCitizen.bat` pour lancer la traduction.
-Une popup vous demandera de choisir le dossier du jeu Star Citizen. /!\ Attention, vous devez selectionner le dossier `data` du jeu, pas le dossier `StarCitizen` lui-même. /!\
-Une fois le dossier selectionné, vous pouvez lancer la traduction avec le bouton `Traduire`. -->
-
 - Téléchargez le projet en cliquant sur le bouton `Code` puis `Download ZIP`.
 - Décompressez le fichier ZIP.
 - Double-cliquez sur le fichier `Traduction_StarCitizen.bat` pour lancer la traduction.
@@ -21,3 +17,12 @@ Bon jeu ! :)
 
 ## Problèmes
 En cas de problème, vous pouvez me contacter sur Discord : `@evan_nbl`
+
+## Prochaines mises à jour
+- Fix des erreurs si on essaye d'ecrire dans un fichier existant
+
+## Crédits
+- [Evan Nbl]
+
+## Licence
+Cette utilitaire n'est pas approuvé par Cloud Imperium ou Roberts Space Industries. Star Citizen®, Squadron 42®, Roberts Space Industries® et Cloud Imperium® sont des marques déposées de Cloud Imperium Rights LLC. Tous droits réservés.
--- traduction.py
+++ traduction.py
@@ -5,10 +5,10 @@ import sys
 import configparser
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk, font
+import zipfile
 
 # Définition des constantes
 DEFAULT_PATH = "C:\\Program Files\\Roberts Space Industries\\StarCitizen\\LIVE\\data"
-URL = "https://traduction.circuspes.fr/fr/global.ini"
 
 # Définition des fonctions
 def get_path():
@@ -23,6 +23,7 @@ def get_path():
         messagebox.showerror("Erreur", "Vous devez sélectionner un dossier")
 
 def get_translation():
+    source = config.get("translation_source", "source")
     path = config.get("path", "path")
     path = path[:-5]
     try:
@@ -31,40 +32,62 @@ def get_translation():
     except Exception as e:
         messagebox.showerror("Erreur", f"Impossible de créer le fichier user.cfg : {str(e)}")
 
-    path = config.get("path", "path")
-    if path:
+    path = os.path.join(path, "data")
+    if source == "Traduction FR Hugo Lisoir":
+        url = "https://traduction.circuspes.fr/fr/global.ini"
         try:
-            urllib.request.urlretrieve(URL, "global.ini")
+            urllib.request.urlretrieve(url, "global.ini")
         except Exception as e:
             messagebox.showerror("Erreur", f"Impossible de récupérer le fichier de traduction : {str(e)}")
+    elif source == "Traduction FR de SPEED0U":
+        # Télécharger le zip depuis GitHub
+        zip_url = "https://github.com/SPEED0U/StarCitizenTranslations/archive/master.zip"
+        try:
+            urllib.request.urlretrieve(zip_url, "translations.zip")
+        except Exception as e:
+            messagebox.showerror("Erreur", f"Impossible de télécharger le fichier de traduction : {str(e)}")
 
-        localization_dir = os.path.join(path, "Localization")
-        if os.path.isdir(localization_dir):
-            french_dir = os.path.join(localization_dir, "french_(france)")
-            if os.path.isfile(french_dir):
-                try:
-                    shutil.copy("global.ini", french_dir)
-                    os.remove("global.ini")
-                except Exception as e:
-                    messagebox.showerror("Erreur", f"Impossible de copier le fichier de traduction : {str(e)}")
-            else:
-                try:
-                    os.mkdir(french_dir)
-                    shutil.copy("global.ini", french_dir)
-                    os.remove("global.ini")
-                except Exception as e:
-                    messagebox.showerror("Erreur", f"Impossible de créer le dossier french_(france) : {str(e)}")
+        # Extraire le zip
+        with zipfile.ZipFile("translations.zip", "r") as zip_ref:
+            zip_ref.extract("StarCitizenTranslations-main/french_(france)/global.ini")
+            shutil.copy("StarCitizenTranslations-main/french_(france)/global.ini", "global.ini")
+
+        os.remove("translations.zip")
+        os.remove("StarCitizenTranslations-main/french_(france)/global.ini")
+        os.rmdir("StarCitizenTranslations-main/french_(france)")
+        os.rmdir("StarCitizenTranslations-main")
+    else:
+        messagebox.showerror("Erreur", "Source de traduction inconnue.")
+        return
+
+    localization_dir = os.path.join(path, "Localization")
+    if os.path.isdir(localization_dir):
+        french_dir = os.path.join(localization_dir, "french_(france)")
+        if os.path.isfile(french_dir):
+            try:
+                shutil.copy("global.ini", french_dir)
+                os.remove("global.ini")
+                messagebox.showinfo("Succès", "La traduction a été appliquée avec succès !")
+            except Exception as e:
+                messagebox.showerror("Erreur", f"Impossible de copier le fichier de traduction : {str(e)}")
         else:
             try:
-                os.mkdir(localization_dir)
-                french_dir = os.path.join(localization_dir, "french_(france)")
                 os.mkdir(french_dir)
                 shutil.copy("global.ini", french_dir)
                 os.remove("global.ini")
+                messagebox.showinfo("Succès", "La traduction a été appliquée avec succès !")
             except Exception as e:
-                messagebox.showerror("Erreur", f"Impossible de créer le dossier Localization : {str(e)}")
+                os.remove("global.ini")
     else:
-        messagebox.showerror("Erreur", "Vous devez sélectionner un dossier")
+        try:
+            os.mkdir(localization_dir)
+            french_dir = os.path.join(localization_dir, "french_(france)")
+            os.mkdir(french_dir)
+            shutil.copy("global.ini", french_dir)
+            os.remove("global.ini")
+            messagebox.showinfo("Succès", "La traduction a été appliquée avec succès !")
+        except Exception as e:
+            messagebox.showerror("Erreur", f"Impossible de créer le dossier Localization : {str(e)}")
 
 # Définition de la fenêtre
 fenetre = tk.Tk()
@@ -89,30 +112,53 @@ if os.path.isfile("config.ini"):
 else:
     config.add_section("path")
     config.set("path", "path", DEFAULT_PATH)
-    with open("config.ini", "w") as configfile:
-        config.write(configfile)
+
+# Ajoutez la section "translation_source" avec la source par défaut si elle n'existe pas
+if not config.has_section("translation_source"):
+    config.add_section("translation_source")
+    config.set("translation_source", "source", "Traduction FR Hugo Lisoir")
+
+# Enregistrez le fichier de configuration
+with open("config.ini", "w") as configfile:
+    config.write(configfile)
 
 # Logo
 fenetre.iconbitmap("logo.ico")
 
 # Définition des widgets
 titre = ttk.Label(fenetre, text="Traduction Star Citizen")
-sous_titre = ttk.Label(fenetre, text="Attention: Sélectionner le dossier data de Star Citizen", foreground="red")
+sous_titre = ttk.Label(fenetre, text="Attention: Sélectionner le dossier 'data' de Star Citizen", foreground="red")
 label_path = ttk.Label(fenetre, text=config.get("path", "path"))
 button_path = ttk.Button(fenetre, text="Sélectionner le dossier", command=get_path)
 button_traduction = ttk.Button(fenetre, text="Appliquer la traduction", command=get_translation, state="disabled")
 
-# Définition de la taille de la fenêtre
-fenetre.geometry("650x200")
+# Créez le sélecteur
+translation_source_label = ttk.Label(fenetre, text="Source de traduction :")
+translation_source_var = tk.StringVar()
+translation_source_combobox = ttk.Combobox(fenetre, textvariable=translation_source_var, values=["Traduction FR Hugo Lisoir", "Traduction FR de SPEED0U"])
+translation_source_combobox.set(config.get("translation_source", "source"))
+translation_source_combobox["width"] = 25
 
 # Placement des widgets
 titre.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
-sous_titre.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
-label_path.grid(row=2, column=0, padx=10, pady=10)
-button_path.grid(row=2, column=1, padx=10, pady=10)
-button_traduction.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+translation_source_combobox.grid(row=0, column=2, padx=10, pady=10)
+sous_titre.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+label_path.grid(row=3, column=0, padx=10, pady=10)
+button_path.grid(row=3, column=1, padx=10, pady=10)
+button_traduction.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
+
+# Définition de la fonction pour mettre à jour la source de traduction
+def update_translation_source(event):
+    source = translation_source_var.get()
+    config.set("translation_source", "source", source)
+    with open("config.ini", "w") as configfile:
+        config.write(configfile)
+
+# Associez la fonction à l'événement de changement d'option dans le sélecteur
+translation_source_combobox.bind("<<ComboboxSelected>>", update_translation_source)
 
 # Lancement de la fenêtre
+fenetre.geometry("850x250")  # Augmentez la hauteur pour faire de la place pour le sélecteur
 fenetre.mainloop()
 
 # Fin du script
